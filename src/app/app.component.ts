b5cdb9ed4b3a7e688ba1d9ea0fd3ce796e789904
sjg99 <38005341+sjg99@users.noreply.github.com>
2019-08-28 21:51:34 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Parcial2808';
+ // title = 'Parcial2808';
+
+ title = 'Parcial';
+ 
+nombre:string = '';
+paises:any = [
+  {nombre: 'Hidrogeno', simbolo:'H'},
+  {nombre: 'Nitrogeno', simbolo:'N', numero_atomico:'7',peso_atomico:'14,0067'},
+  {nombre: 'Calcio', simbolo:'Ca', numero_atomico:'20',peso_atomico:'40,078'}
+
+ 
+]
+
+//simbolo:string='';
+//numero_atomico:string='';
+//peso_atomico:string='';
+
 }
